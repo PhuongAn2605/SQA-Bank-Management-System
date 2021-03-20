7349ca2bf229b59739bf345a2c7ff791538fc14a
PhuongAn2605 <anphuong2605@gmail.com>
2021-03-20 21:59:23 +0700

    fix UI

---
+++ Source Code/README.md
@@ -0,0 +1,11 @@
+# Banking-Management-System
+
+# Before running the application, there are few things that you need to do: 
+
+- Install MongoDB tools for restoring data: https://www.mongodb.com/try/download/database-tools
+
+- Install node modules and all dependencies 
+
+- I will make a video to show you how to restore data. The vid should be available in the package i send.
+
+Good luck! Have fun!
---
+++ Source Code/common.js
@@ -0,0 +1,51 @@
+var fs = require("fs")
+var MongoClient = require("mongodb").MongoClient
+
+class Common {
+
+    _dbo = null
+
+    constructor() {
+        var self = this
+        MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, function(err, client) {
+            if (err) throw err
+            self._dbo = client.db("SQA") // select the database
+            console.log("DB connected!")
+        })
+        // this.connect(self)
+    }
+
+    getDb() {
+        return this._dbo;
+    }
+
+    randStr(len) {
+        let s = '';
+        while (s.length < len) s += Math.random().toString(36).substr(2, len - s.length)
+        return s
+    }
+
+    // connect(self){
+    //     MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, function(err, client) {
+    //         if (err) throw err
+    //         self._dbo = client.db("SQA") // select the database
+    //         console.log("DB connected!")
+    //     })
+    // }
+
+    // close() {
+    //     return MongoClient.
+    //   }
+
+    async render(res) {
+        await fs.readFile(res.viewpath, 'utf8', function(err, data) {
+            const keys = Object.keys(res.parts)
+            keys.forEach(function(item) {
+                data = data.replace("{" + item + "}", res.parts[item])
+            })
+            res.send(data)
+        })
+    }
+}
+
+module.exports = new Common()
