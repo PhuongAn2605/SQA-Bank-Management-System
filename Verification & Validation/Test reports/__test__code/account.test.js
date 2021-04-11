695002797829427896fb30782de8dbff50e3d70a
PhuongAn2605 <anphuong2605@gmail.com>
2021-04-11 16:05:39 +0700

    add test report

---
+++ Verification & Validation/Test reports/__test__code/account.test.js
@@ -0,0 +1,316 @@
+const request = require('supertest');
+const { app } = require('../server');
+var database = require("../database")
+
+
+const { MongoClient, ObjectId } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+// const { response, response } = require('express');
+
+let accountId;
+
+const testAccount = {
+  role: "user",
+  cardNo: "12345678901236",
+  username: "PhuongAn",
+  password: "12345678",
+  address: "HD",
+  dob: "2000-05-26",
+  phone: "084834232",
+  email: "anphuong2605@gmail.com",
+  balance: 0
+}
+
+function serialise(obj) {
+  return Object.keys(obj)
+    .map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(obj[k])}`)
+    .join('&');
+}
+
+describe('TEST ACCOUNT', () => {
+  let connection;
+  let account;
+
+  beforeAll(async () => {
+    mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
+    connection = await MongoClient.connect(mongoUri, {
+      useNewUrlParser: true,
+    });
+    account = await database.getDb().collection("account");
+    await account.deleteMany({});
+
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    // await db.close();
+    await database.close();
+  });
+
+  it('should insert an account into collection', async () => {
+    // account = database.getDb().collection("account");
+
+    const mockAccount = {
+      role: 'user',
+      cardNo: '12345678901235',
+      username: "Phuongzz",
+      password: "12345678",
+      address: "HD",
+      dob: "2000-09-08",
+      phone: "084834232",
+      email: "anphuong2605@gmail.com",
+      balance: "0"
+    };
+    await account.insertOne(mockAccount);
+
+    const insertedAccount = await account.findOne({ cardNo: mockAccount["cardNo"] });
+    // console.log(insertedAccount);
+    // accountId = String(insertedAccount._id);
+    // console.log( " PHUONG" + accountId);
+    expect(insertedAccount).toStrictEqual(mockAccount);
+  });
+
+  it("GET ACCOUNT-LIST", async () => {
+    const response = await request(app).get('/account_list')
+    // console.log(response);
+    // console.log(request);
+    expect(response.type).toEqual(expect.stringMatching('text/html'));
+    expect(response.status).toEqual(200);
+
+  });
+
+  //not done yet
+  it('GET ACCOUNT-CREATE', async () => {
+    const response = await request(app).get('/account_create');
+    // console.log(response);
+    expect(response.type).toEqual(expect.stringMatching('text/html'));
+    expect(response.status).toEqual(200);
+  });
+
+
+  describe("POST ACCOUNT-CREATE", () => {
+    describe("POST ACCOUNT-CREATE - Valid Account", () => {
+      it("Should add the account to database", async () => {
+        app.set('body', testAccount);
+        const response = await request(app).post('/account_create')
+          .send(serialise(testAccount));
+        // console.log(response.statusCode);
+        // console.log("SERIALISE " + serialise(testAccount));
+        expect(response.statusCode).toEqual(201);
+
+        const insertTestAccount = await account.findOne({ cardNo: testAccount["cardNo"] });
+
+        accountId = String(insertTestAccount._id);
+
+        expect(insertTestAccount["cardNo"]).toStrictEqual(testAccount["cardNo"]);
+        expect(insertTestAccount.username).toStrictEqual(testAccount.username);
+        expect(insertTestAccount.phonenumber).toStrictEqual(testAccount.phonenumber);
+
+        // done();
+      });
+    });
+
+    describe("POST ACCOUNT-CREATE - Invalid password (password.length < 6 || password.length > 32)", () => {
+      let invalidPassAccount = {
+        role: "user",
+        cardNo: "12345678901234",
+        username: "PhuongAA",
+        password: "",
+        address: "HD",
+        dob: "2000-09-08",
+        phone: "084834232",
+        email: "anphuong2605@gmail.com",
+        balance: 0
+      }
+      it("Not add the account to db due to password.lenghth = 0", async () => {
+        app.set('body', invalidPassAccount);
+        const response = await request(app).post('/account_create')
+          .send(serialise(invalidPassAccount));
+        // console.log(response.statusCode);
+        expect(response.statusCode).toEqual(400);
+
+        const insertTestAccount = await account.findOne({ cardNo: invalidPassAccount["cardNo"] });
+        expect(insertTestAccount).toBeNull();
+        // done();
+
+      });
+
+      it("Not add the account to db due to password.lenghth = 2", async () => {
+        invalidPassAccount.password = "12";
+        app.set('body', invalidPassAccount);
+        const response = await request(app).post('/account_create')
+          .send(serialise(invalidPassAccount));
+        // console.log(response.statusCode);
+        expect(response.statusCode).toEqual(400);
+
+        const insertTestAccount = await account.findOne({ cardNo: invalidPassAccount["cardNo"] });
+        expect(insertTestAccount).toBeNull();
+        // done();
+
+      });
+
+      it("Not add the account to db due to password.lenghth = 33", async () => {
+        invalidPassAccount.password = "123456123456123456123456123456123456";
+        app.set('body', invalidPassAccount);
+        const response = await request(app).post('/account_create')
+          .send(serialise(invalidPassAccount));
+        // console.log(response.statusCode);
+        expect(response.statusCode).toEqual(400);
+
+        const insertTestAccount = await account.findOne({ cardNo: invalidPassAccount["cardNo"] });
+        expect(insertTestAccount).toBeNull();
+        // done();
+
+      });
+    });
+
+    describe("POST - ACCOUNT-CREATE - Invalid username (username.length <3 || username.length >32)", () => {
+      let invalidNameAccount = {
+        role: "user",
+        cardNo: "12345678901234",
+        username: "P",
+        password: "12345678",
+        address: "HD",
+        dob: "2000-09-08",
+        phone: "084834232",
+        email: "anphuong2605@gmail.com",
+        balance: 0
+      }
+      it("Not add the account to db due to username.length = 1", async () => {
+        app.set('body', invalidNameAccount);
+        const response = await request(app).post('/account_create')
+          .send(serialise(invalidNameAccount));
+        // console.log(response.statusCode);
+        expect(response.statusCode).toEqual(400);
+
+        const insertTestAccount = await account.findOne({ username: invalidNameAccount["username"] });
+        expect(insertTestAccount).toBeNull();
+      });
+      it("Not add the account to db due to username.length = 0", async () => {
+        invalidNameAccount.username = "";
+        app.set('body', invalidNameAccount);
+        const response = await request(app).post('/account_create')
+          .send(serialise(invalidNameAccount));
+        // console.log(response.statusCode);
+        expect(response.statusCode).toEqual(400);
+
+        const insertTestAccount = await account.findOne({ username: invalidNameAccount["username"] });
+        expect(insertTestAccount).toBeNull();
+      });
+
+      it("Not add the account to db due to username.length = 34", async () => {
+        invalidNameAccount.username = "1234561234561234561234561234561234561";
+        app.set('body', invalidNameAccount);
+        // console.log("BODY" + request.body);
+        const response = await request(app).post('/account_create')
+          .send(serialise(invalidNameAccount));
+        // console.log(response.statusCode);
+        expect(response.statusCode).toEqual(400);
+
+        const insertTestAccount = await account.findOne({ username: invalidNameAccount["username"] });
+        expect(insertTestAccount).toBeNull();
+      });
+    });
+  })
+
+  describe("GET ACCOUNT-EDIT", () => {
+    it("Should return HTML as EDIT AN ACCOUNT PAGE", async () => {
+      // console.log(accountId);
+      // app.set('params', accountId);
+      // console.log(" PARAM " + request.params);
+      const response = await request(app).get("/account_edit_" + accountId)
+      expect(response.type).toEqual(expect.stringMatching('text/html'));
+
+    });
+  });
+
+  describe("POST ACCOUNT-EDIT", () => {
+    it("Should update the account", async () => {
+      // console.log(accountId);
+      testAccount.username = "Test";
+      app.set('body', testAccount);
+      // console.log(" PARAM " + request.params);
+      const response = await request(app).post("/account_edit_" + accountId)
+      .send(serialise(testAccount))
+      expect(response.statusCode).toEqual(200);
+
+      const updateAccount = await account.findOne({_id: ObjectId(accountId)});
+      expect(updateAccount.username).toEqual(expect.stringMatching(testAccount.username));
+      expect(updateAccount.cardNo).toEqual(testAccount.cardNo);
+
+    });
+
+    it("Should return the error - Account with the id can not be found", async () => {
+      // console.log(accountId);
+      // testAccount.username = "Test";
+      let testUserId = "6070f5accf05e567f83576c2";
+      // app.set('body', testAccount);
+      // console.log(" PARAM " + request.params);
+      const response = await request(app).post("/account_edit_" + testUserId)
+      // .send(serialise(testAccount))
+
+      const updateAccount = await account.findOne({_id: ObjectId(testUserId)});
+      expect(updateAccount).toBeNull();
+      expect(response.statusCode).toEqual(400);
+      expect(response.text).toEqual(expect.stringContaining("Account with id"));
+      expect(response.text).toEqual(expect.stringContaining("cannot be found!"));
+
+    });
+
+    it("Should return the error - Username length is not valid", async () => {
+      // console.log(accountId);
+      testAccount.username = "PP";
+      app.set('body', testAccount);
+      // console.log(" PARAM " + request.params);
+      const response = await request(app).post("/account_edit_" + accountId)
+      .send(serialise(testAccount))
+      // expect(response.statusCode).toEqual(200);
+
+      const updateAccount = await account.findOne({_id: ObjectId(accountId)});
+      expect(updateAccount.cardNo).toEqual(testAccount.cardNo)
+      expect(response.statusCode).toEqual(400);
+      expect(response.text).toEqual(expect.stringContaining("Username length is not valid"));
+    });
+
+    it("Should return error Username has been used already", async () => {
+      // console.log(accountId);
+      testAccount.username = "Phuongzz";
+      app.set('body', testAccount);
+      // console.log(" PARAM " + request.params);
+      const response = await request(app).post("/account_edit_" + accountId)
+      .send(serialise(testAccount))
+      // expect(response.statusCode).toEqual(200);
+
+      // const updateAccount = await account.findOne({_id: ObjectId(accountId)});
+      // expect(updateAccount.cardNo).toEqual(testAccount.cardNo)
+      expect(response.statusCode).toEqual(400);
+      expect(response.text).toEqual(expect.stringContaining("has been used already"));
+    });
+
+    it("Should return the error - Password length is not valid", async () => {
+      // console.log(accountId);
+      testAccount.username = "Test";
+      testAccount.password = "12";
+      app.set('body', testAccount);
+      // console.log(" PARAM " + request.params);
+      const response = await request(app).post("/account_edit_" + accountId)
+      .send(serialise(testAccount))
+      // expect(response.statusCode).toEqual(200);
+
+      // const updateAccount = await account.findOne({_id: ObjectId(accountId)});
+      // expect(updateAccount.cardNo).toEqual(testAccount.cardNo)
+      expect(response.statusCode).toEqual(400);
+      expect(response.text).toEqual(expect.stringContaining("Password length is not valid"));
+    });
+  })
+
+  describe("GET ACCOUNT-DELETE", () => {
+    it("Should delete the account", async () => {
+      const response = await request(app).get("/account_delete_" + accountId)
+      expect(response.statusCode).toEqual(302);
+    });
+
+  });
+});
+
---
+++ Verification & Validation/Test reports/__test__code/authentication.test.js
@@ -0,0 +1,320 @@
+const request = require('supertest');
+const { app } = require('../server.js');
+var database = require("../database");
+
+
+
+const { MongoClient, ObjectId } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let mockUser = {
+    role: 'user',
+    cardNo: '1234567890000',
+    username: "Test-login",
+    password: "12345678",
+    address: "HN",
+    dob: "2000-09-08",
+    phone: "084834232",
+    email: "anphuong2605@gmail.com",
+    balance: "0"
+}
+
+let mockUserSignup = {
+    role: 'user',
+    cardNo: '1234567890002',
+    username: "Test",
+    password: "12345678",
+    address: "HN",
+    dob: "2000-09-08",
+    phone: "084834232",
+    email: "anphuong2605@gmail.com",
+    // balance: "0"
+}
+
+let mockAdmin = {
+    username: "quocdat",
+    password: "123456",
+    role: "admin",
+    address: "HN",
+    dob: "2000-09-08",
+    phone: "084834232",
+    email: "sadinh1106@gmail.com",
+    cardNo: "1801040061",
+    balance: "0"
+}
+
+let testUserId;
+let testAdminId;
+
+function serialise(obj) {
+    return Object.keys(obj)
+        .map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(obj[k])}`)
+        .join('&');
+}
+
+describe('TEST AUTHENTICATION', () => {
+    let connection;
+    let account;
+
+    beforeAll(async done  => {
+        mongo = new MongoMemoryServer();
+        const mongoUri = await mongo.getUri();
+        connection = await MongoClient.connect(mongoUri, {
+            useNewUrlParser: true,
+        });
+        account = await database.getDb().collection("account");
+        await account.deleteMany({});
+        done();
+        // window.document.cookie.login = "j%3A%22606c44f392060d1d88964c06%22";
+    });
+
+    // afterEach(async () => {
+    //     await account.deleteMany({});
+    // })
+
+    afterAll(async () => {
+        await connection.close();
+        // await db.close();
+        await database.close();
+    });
+
+
+    it("GET LOG IN should return status 200'", async () => {
+        const response = await request(app).get('/login');
+        // console.log(response)
+        expect(response.statusCode).toBe(200);
+
+    });
+
+    //not done yet
+    // describe("LOGIN ADMIN", () => {
+    //     it("Login with valid username and password", async () => {
+    //         await account.insertOne(mockAdmin);
+
+    //         const insertedAccount = await account.findOne({ cardNo: mockAdmin["cardNo"] });
+    //         expect(insertedAccount).toStrictEqual(mockAdmin);
+    //         expect(insertedAccount.cardNo).toEqual(mockAdmin.cardNo);
+    //         expect(insertedAccount.username).toEqual(mockAdmin.username);
+
+
+    //         testAdminId = insertedAccount._id;
+
+    //         app.set('body', mockAdmin);
+    //         let response = await request(app).post('/login')
+    //             .send(serialise(mockAdmin))
+    //             .expect(302);
+
+    //         // const response = request(app).get('/account_list')
+    //         //     .set("Cookie", data.body._id)
+    //         //     .expect(201);
+
+    //         //     console.log(data.body._id)
+    //     });
+    //     it("Login with invalid username, it should return an error", async () => {
+    //         mockAdmin.username = "";
+    //         // let query = { "_id": testAdminId };
+    //         // await account.updateOne(query, { $set: mockAdmin })
+    //         app.set('body', mockAdmin);
+    //         let response = await request(app).post('/login')
+    //             .send(serialise(mockAdmin));
+    //         // console.log(response.text);
+    //         expect(response.text).toEqual(expect.stringContaining('Incorrect username'));
+    //         expect(response.statusCode).toEqual(400);
+    //     });
+
+    //     it("Login with invalid password, it should return an error", async () => {
+    //         mockAdmin.password = "";
+    //         mockAdmin.username = "quocdat";
+    //         app.set('body', mockAdmin);
+    //         let response = await request(app).post('/login')
+    //             .send(serialise(mockAdmin));
+    //         // console.log(response.text);
+    //         expect(400);
+    //         expect(response.text).toEqual(expect.stringContaining('Incorrect password'));
+    //     });
+
+    //     it("Login with wrong password, it should return an error", async () => {
+    //         mockAdmin.password = "632763";
+    //         mockAdmin.username = "quocdat";
+    //         app.set('body', mockAdmin);
+    //         let response = await request(app).post('/login')
+    //             .send(serialise(mockAdmin));
+    //         // console.log(response.text);
+    //         expect(400);
+    //         expect(response.text).toEqual(expect.stringContaining('Incorrect password'));
+    //     });
+    // });
+
+    // //not done yet
+    // describe("POST LOGIN USER", () => {
+    //     it("Login with valid information", async () => {
+    //         await account.insertOne(mockUser);
+
+    //         const insertedUser = await account.findOne({ cardNo: mockUser["cardNo"] });
+    //         expect(insertedUser.username).toStrictEqual(mockUser.username);
+    //         expect(insertedUser.cardNo).toStrictEqual(mockUser.cardNo);
+
+
+    //         testUserId = insertedUser._id;
+
+    //         app.set('body', mockUser);
+    //         let response = await request(app).post('/login')
+    //             .send(serialise(mockUser))
+    //             //    expect('Content-Type', /json/)
+    //             expect(response.statusCode).toEqual(302);
+    //         // await account.deleteMany({});
+    //     })
+
+    //     it("Login with invalid username", async () => {
+    //         mockUser.username = "";
+
+    //         app.set('body', mockUser);
+    //         let response = await request(app).post('/login')
+    //             .send(serialise(mockUser));
+    //         // console.log(data);
+    //         //  console.log(response.text);
+    //         expect(400);
+    //         expect(response.text).toEqual(expect.stringContaining('Incorrect username'));
+    //         // await account.deleteMany({});
+
+    //     });
+
+    //     it("Login with wrong username", async () => {
+    //         mockUser.username = "phuongan";
+    //         app.set('body', mockUser);
+    //         let response = await request(app).post('/login')
+    //             .send(serialise(mockUser));
+    //         // console.log(data);
+    //         //  console.log(response.text);
+    //         expect(400);
+    //         expect(response.text).toEqual(expect.stringContaining('Incorrect username'));
+    //         // await account.deleteMany({});
+
+    //     });
+
+    //     it("Login with invalid password", async () => {
+    //         mockUser.username = "Test-login";
+    //         mockUser.password = "12";
+    //         app.set('body', mockUser);
+    //         // console.log(mockUser);
+    //         let response = await request(app).post('/login')
+    //             .send(serialise(mockUser));
+    //         // console.log(data);
+    //         //  console.log(response.text);
+    //         expect(400);
+    //         expect(response.text).toEqual(expect.stringContaining('Incorrect password'));
+
+    //     });
+
+    //     it("Login with invalid password and username", async () => {
+    //         mockUser.username = "phuongan";
+    //         mockUser.password = "12454";
+    //         app.set('body', mockUser);
+    //         // console.log(mockUser);
+    //         let response = await request(app).post('/login')
+    //             .send(serialise(mockUser));
+    //         // console.log(data);
+    //         //  console.log(response.text);
+    //         expect(400);
+    //         expect(response.text).toEqual(expect.stringContaining('Incorrect username'));
+
+    //     });
+    // })
+
+    // it("GET HOMEPAGE", async () => {
+    //     const response = await request(app).get('/home_page');
+    //     // console.log(response)
+    //     expect(response.statusCode).toBe(200);
+    // });
+
+    // it("GET ADMIN", async () => {
+    //     const response = await request(app).get('/admin');
+    //     // console.log(response)
+    //     expect(response.statusCode).toBe(200);
+    // });
+
+
+    // it("GET SIGN UP", async () => {
+    //     const response = await request(app).get('/sign_up');
+    //     // console.log(response);
+    //     expect(response.statusCode).toBe(200);
+    // });
+
+    // describe("POST SIGN-UP", () => {
+    //     it("Should return return status 201 - OK", async () => {
+    //         app.set('body', mockUserSignup);
+    //         const response = await request(app).post("/sign_up")
+    //         .send(serialise(mockUserSignup));
+    //         expect(response.status).toEqual(201);
+    //         // console.log(response);
+    //     });
+
+    //     it("Should return an error and status 400- BAD REQUEST due to invalid username", async () => {
+    //         mockUserSignup.username ="PP";
+    //         app.set('body', mockUserSignup);
+    //         const response = await request(app).post("/sign_up")
+    //         .send(serialise(mockUserSignup));
+    //         expect(response.statusCode).toEqual(400);
+    //         expect(response.text).toEqual(expect.stringContaining("Name length isn't valid"));
+    //         // console.log(response);
+    //     });
+
+    //     it("Should return an error and status 400- BAD REQUEST due to exsiting username", async () => {
+    //         mockUserSignup.username ="quocdat";
+    //         app.set('body', mockUserSignup);
+    //         const response = await request(app).post("/sign_up")
+    //         .send(serialise(mockUserSignup));
+    //         expect(response.statusCode).toEqual(400);
+    //         expect(response.text).toEqual(expect.stringContaining("Name already existed"));
+    //         // console.log(response);
+    //     });
+
+    //     it("Should return an error and status 400- BAD REQUEST due to invalid password", async () => {
+    //         mockUserSignup.username ="Test";
+    //         mockUserSignup.password = "aa";
+    //         app.set('body', mockUserSignup);
+    //         const response = await request(app).post("/sign_up")
+    //         .send(serialise(mockUserSignup));
+    //         expect(response.statusCode).toEqual(400);
+    //         expect(response.text).toEqual(expect.stringContaining("Password length is not valid"));
+    //         // console.log(response);
+    //     });
+
+    // });
+
+    // // describe("GET PROFILE", () => {
+
+    // //     // app.set('body', mockUser);
+    // //     //     let response = await request(app).post('/login')
+    // //     //         .send(serialise(mockUser))
+    // //     //         //    expect('Content-Type', /json/)
+    // //     //         expect(response.statusCode).toEqual(302);
+    // //         // await account.deleteMany({});
+
+    // //     it("Should retun Profile page", async () => {
+    // //         mockUser.username = "Test-login";
+    // //         mockUser.password = "12345678";
+
+    // //         app.set('body', mockUser);
+    // //         let response = await request(app).post('/login')
+    // //             .send(serialise(mockUser))
+    // //             //    expect('Content-Type', /json/)
+    // //             expect(response.statusCode).toEqual(302);
+    // //             console.log(re.body);
+    // //         let testLogin = {
+    // //             login: testUserId
+    // //         }
+    // //         app.set('cookies', testLogin);
+    // //         // console.log(request);
+    // //         // console.log(request.cookies);
+    // //         const response2 = await request(app).get('/profile')
+    // //         // .send(serialise(testLogin))
+    // //     });
+    // // });
+
+    // it("GET SIGN OUT", async () => {
+    //     const response = await request(app).get('/sign_out');
+    //     // console.log(response);
+    //     expect(response.statusCode).toBe(302);
+    // });
+});
